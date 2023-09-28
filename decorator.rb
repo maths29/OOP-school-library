51ceacf364800236a3a72f59e950cae6e10bbb11
mathsoft29 <iwuconcept@gmail.com>
2023-09-28 09:33:27 +0100

    Add decorator to the project

---
+++ decorator.rb
@@ -0,0 +1,14 @@
+required_relative 'nameable'
+
+class Decorator < Nameable
+  attr_reader :nameable
+
+  def initialize(nameable)
+    @nameable = nameable
+    super()
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
