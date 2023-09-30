193a2a890eab76562713199911457dd4ad486fb3
mathsoft29 <iwuconcept@gmail.com>
2023-09-30 19:55:43 +0100

    Fix the required changes

--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,15 @@
-class Teacher < Person
-  attr_reader :specialization
+require_relative 'person'
 
-  def initialize(name, age, specialization, _parent_permission)
-    super(age, name)
+class Teacher < Person
+  def initialize(specialization, age, name, parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
+
+  attr_reader :specialization
 
   def can_use_services?
     true
   end
 end
+
