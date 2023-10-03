f160b443be4acf2c120121a48d38881f2d4502b7
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-10-03 12:51:25 +0300

    Put menu in main.rb

--- app.rb
+++ app.rb
@@ -31,22 +31,6 @@ class App
     end
   end
 
-  def start
-    user_input = 0
-    while user_input != 7
-      puts 'Please choose an option by entering a number:'
-      puts '1 - List all books'
-      puts '2 - List all person'
-      puts '3 - Create a person'
-      puts '4 - Create a book'
-      puts '5 - Create a rental'
-      puts '6 - List all rentals for a given person id'
-      puts '7 - Exit'
-      user_input = gets.chomp.strip.to_i
-      case_method(user_input)
-    end
-  end
-
   def list_books
     @books.each_with_index do |book, index|
       puts "#{index} Title : #{book.title}, Author : #{book.author}"
--- main.rb
+++ main.rb
@@ -2,7 +2,19 @@ require_relative 'app'
 
 def main
   app = App.new
-  app.start
+  user_input = 0
+  while user_input != 7
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all person'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+    user_input = gets.chomp.strip.to_i
+    app.case_method(user_input)
+  end
 end
 
 main
