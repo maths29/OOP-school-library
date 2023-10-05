d04d60f13360b2930d3ba8bd576ce4c45066e384
mathsoft29 <iwuconcept@gmail.com>
2023-10-05 08:31:14 +0100

    preserve person data

--- lib/app.rb
+++ lib/app.rb
@@ -7,14 +7,13 @@ require_relative 'capitalize'
 require_relative 'trimcase'
 require_relative 'rental'
 require 'json'
-
 class App
   def initialize
-    @books = []
-    @person = []
+    load_books
+    load_persons
+    @people = []
     @rentals = []
   end
-
   def case_method(user_input)
     case user_input
     when 1
@@ -31,19 +30,34 @@ class App
       list_rentals
     end
   end
-
   def list_books
     @books.each_with_index do |book, index|
       puts "#{index} Title : #{book.title}, Author : #{book.author}"
     end
   end
-
-  def list_person
-    @person.each_with_index do |person, index|
-      puts "#{index} [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+  def load_books
+    @books = []
+    if File.file?('./lib/books.json')
+      book_data = JSON.parse(File.read('./lib/books.json'))
+      book_data.each do |book_info|
+        @books << Book.new(book_info['title'], book_info['author'])
+      end
     end
   end
-
+  def load_persons
+    @person = []
+    if File.file?('./lib/persons.json')
+      person_data = JSON.parse(File.read('./lib/persons.json'))
+      person_data.each do |person_info|
+        @person << Person.new(person_info['age'], person_info['name'], person_info['user_type'])
+      end
+    end
+  end
+  def list_person
+    @person.each_with_index do |person, index|
+      puts "#{index} [#{person.user_type}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+  end
   def create_person
     print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     user_type = gets.chomp.strip.to_i
@@ -51,82 +65,106 @@ class App
     age = gets.chomp.strip.to_i
     print 'Name: '
     name = gets.chomp.strip
+    existing_persons = []
     if user_type == 1
       print 'Has parent permission? [Y/N]: '
       parent_permission = gets.chomp.strip.upcase == 'Y'
-      @person << Students.new(name, age, parent_permission)
+      student = Student.new(name, age, user_type, parent_permission)
+      existing_persons = JSON.parse(File.read('./lib/persons.json')) if File.file?('./lib/persons.json')
+      existing_persons << {
+        id: student.id,
+        name: student.name,
+        age: student.age,
+        permission: student.parent_permission,
+        user_type: "#{student.class}"
+      }
+      @person << student
     else
       print 'Specialization: '
       specialization = gets.chomp.strip
-      @person << Teacher.new(name, age, specialization)
+      teacher = Teacher.new(name, age, user_type, specialization)
+      @person << teacher
+      existing_persons = JSON.parse(File.read('./lib/persons.json')) if File.file?('./lib/persons.json')
+      existing_persons << {
+        id: teacher.id,
+        name: teacher.name,
+        age: teacher.age,
+        specialization: teacher.specialization,
+        user_type: "#{teacher.class}"
+      }
     end
+    File.write('./lib/persons.json', JSON.pretty_generate(existing_persons))
     puts 'Person created successfully'
   end
-
   def add_book
     print 'Title: '
-  title = gets.chomp.strip
-  print 'Author: '
-  author = gets.chomp.strip
-  @books << Book.new(title, author)
-
-  # Serialize @books array to JSON
-  serialized_books = @books.map { |book| { title: book.title, author: book.author } }
-
-  File.open('./lib/books.json', 'w') do |file|
-    file.write(JSON.pretty_generate(serialized_books))
+    title = gets.chomp.strip
+    print 'Author: '
+    author = gets.chomp.strip
+    new_book = Book.new(title, author)
+    @books << new_book
+    # Read existing data from the file if it exists
+    existing_books = []
+    existing_books = JSON.parse(File.read('./lib/books.json')) if File.file?('./lib/books.json')
+    # Append the new book data to the existing data
+    existing_books << { title: new_book.title, author: new_book.author }
+    # Write the combined data back to the file
+    File.write('./lib/books.json', JSON.pretty_generate(existing_books))
+    puts 'Book created successfully'
   end
-
-  puts 'Book created successfully'
-  end
-
   def person_index_val
     puts 'Select person from the following list by number'
     @person.each_with_index do |persons, index|
-      person_type = persons.instance_of?(Students) ? 'Student' : 'Teacher'
-      puts "#{index})[#{person_type}] Name: #{persons.name}, ID: #{persons.id}, Age: #{persons.age}"
+      #person_type = persons.instance_of?(Students) ? 'Student' : 'Teacher'
+      puts "#{index})[#{persons.user_type}] Name: #{persons.name}, ID: #{persons.id}, Age: #{persons.age}"
     end
-
     gets.chomp.to_i
   end
-
   def accept_book_index
     puts 'Select a book from the following list by number'
     @books.each_with_index do |book, index|
       puts "#{index}) Title: #{book.title}, Author: #{book.author}"
     end
-
     book_index = gets.chomp.to_i
-
     until book_index >= 0 && book_index < @books.length
       puts 'Please enter a valid index:'
       book_index = gets.chomp.to_i
     end
-
     book_index
   end
-
   def create_rental
     book_index = accept_book_index
     person_index = person_index_val
-
     print 'Date: '
     date = gets.chomp
-    Rental.new(date, @person[person_index], @books[book_index])
+    rental = Rental.new(date, @person[person_index], @books[book_index])
+    # Read existing data from the file if it exists
+    existing_rentals = []
+    if File.file?('./lib/rentals.json')
+      existing_rentals = JSON.parse(File.read('./lib/rentals.json'))
+    end
+    # Append the new rental data to the existing data
+    existing_rentals << {
+      date: rental.date,
+      person_id: rental.person.id,
+      book_title: rental.book.title,
+      book_author: rental.book.author
+    }
+    # Write the combined data back to the file
+    File.open('./lib/rentals.json', 'w') do |file|
+      file.write(JSON.pretty_generate(existing_rentals))
+    end
     puts 'Rental created successfully'
   end
-
   def get_rental_id(person_id)
     person = @person.find { |borrowedb| borrowedb.id == person_id }
     person ? person.rentals : []
   end
-
   def list_rentals
     print 'ID of person : '
     id = gets.chomp.to_i
     choose_person = get_rental_id(id)
     puts 'Rentals: '
-
     choose_person.each do |rental|
       puts "Date: \"#{rental.date}\", Book: #{rental.book.title} by #{rental.book.author}"
     end
--- lib/person.rb
+++ lib/person.rb
@@ -6,53 +6,49 @@ require_relative 'rental'
 class Person < Nameable
   # constructor method
   attr_reader :id, :rentals
-  attr_accessor :name, :age
-
-  def initialize(age, name = 'unknown', parent_permission: true)
+  attr_accessor :name, :age, :user_type
+  def initialize(age, name, user_type, parent_permission: true)
     @name = name
     @age = age
+    @user_type = user_type
     @id = Random.rand(1..1000)
     @parent_permission = parent_permission
     @rentals = []
     super()
   end
-
   # getter methods
   def getid
     @id
   end
-
   def getname
     @name
   end
-
   def getage
     @age
   end
-
+  def gettype
+    @user_type
+  end
   # setter methods
   def setname=(val)
     @name = val
   end
-
   def setage=(val)
     @age = val
   end
-
+  def settype=(val)
+    @user_type
+  end
   def can_use_services?
     of_age? || @parent_permission
   end
-
   def correct_name
     @name
   end
-
   def rentbook(date, book)
     Rental.new(date, book, self)
   end
-
   private
-
   def of_age?
     @age >= 18
   end
